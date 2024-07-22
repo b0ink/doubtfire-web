988a1bde2936b4d58615c782c382700674d31ef5
b0ink <40929320+b0ink@users.noreply.github.com>
2024-07-22 20:23:02 +1000

    refactor: enforce type safety for data passed to comments-modal component

--- src/app/common/modals/comments-modal/comments-modal.component.ts
+++ src/app/common/modals/comments-modal/comments-modal.component.ts
@@ -1,5 +1,11 @@
 import {Component, Input, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {TaskComment} from 'src/app/api/models/doubtfire-model';
+
+export interface CommentsModalData {
+  comment: TaskComment;
+  commentResourceUrl: string;
+}
 
 @Component({
   selector: 'comments-modal',
@@ -7,13 +13,13 @@ import {MAT_DIALOG_DATA} from '@angular/material/dialog';
   styleUrls: ['./comments-modal.component.scss'],
 })
 export class CommentsModalComponent implements OnInit {
-  @Input() commentType: string;
+  @Input() taskComment: TaskComment;
   @Input() commentResourceUrl: string;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: CommentsModalData) {}
 
   ngOnInit(): void {
-    this.commentType = this.data.commentType;
+    this.taskComment = this.data.comment;
     this.commentResourceUrl = this.data.commentResourceUrl;
   }
 }
--- src/app/common/modals/comments-modal/comments-modal.service.ts
+++ src/app/common/modals/comments-modal/comments-modal.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
-import {CommentsModalComponent} from './comments-modal.component';
+import {TaskComment} from 'src/app/api/models/doubtfire-model';
+import {CommentsModalComponent, CommentsModalData} from './comments-modal.component';
 
 @Injectable({
   providedIn: 'root',
@@ -8,9 +9,9 @@ import {CommentsModalComponent} from './comments-modal.component';
 export class CommentsModalService {
   constructor(public dialog: MatDialog) {}
 
-  public show(commentResourceUrl: string, commentType: string) {
-    this.dialog.open(CommentsModalComponent, {
-      data: {commentResourceUrl, commentType},
+  public show(commentResourceUrl: string, comment: TaskComment) {
+    this.dialog.open<CommentsModalComponent, CommentsModalData>(CommentsModalComponent, {
+      data: {commentResourceUrl, comment},
       width: '100%',
       maxWidth: '900px',
     });
